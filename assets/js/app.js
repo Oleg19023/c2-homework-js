739633a0c853c5ea3b8a9152589a55834ecf68f8
Oleg19023 <96693781+Oleg19023@users.noreply.github.com>
2023-12-22 22:06:23 +0200

    commit

---
+++ assets/js/app.js
@@ -0,0 +1,54 @@
+// Аннуитетный калькуль
+function calculateLoan() {
+    // Ввод
+    const loanTermMonths = parseFloat(document.getElementById('loanTermMonths').value);
+    const loanAmount = parseFloat(document.getElementById('loanAmount').value);
+    const annualInterestRate = parseFloat(document.getElementById('annualInterestRate').value);
+
+    // Расчет месяц-платеж
+    const monthlyInterestRate = (annualInterestRate / 100) / 12;
+    let monthlyPayment = loanAmount * monthlyInterestRate / (1 - Math.pow(1 + monthlyInterestRate, -loanTermMonths));
+    let totalPayment = 0;
+    let totalPrincipalPayment = 0;
+    let totalInterestPayment = 0;
+    let balance = loanAmount;
+
+    const resultContainer = document.getElementById('resultContainer');
+    resultContainer.innerHTML = '';
+
+    // Табличка 
+    const table = document.createElement('table');
+    table.className = 'table';
+    table.innerHTML = '<tr><th>Месяц</th><th>Задолженность по кредиту</th><th>Погашение кредита</th><th>Проценты по кредиту</th><th>Выплаты в месяц</th><th>Остаток долга</th></tr>';
+    resultContainer.appendChild(table);
+
+    // Расчет-вывод погаш. кредита
+    for (let i = 1; i <= loanTermMonths; i++) {
+        let initialBalance = balance;
+        let interestPayment = balance * monthlyInterestRate;
+        let principalPayment = monthlyPayment - interestPayment;
+        balance -= principalPayment;
+        totalPayment += monthlyPayment;
+        totalPrincipalPayment += principalPayment;
+        totalInterestPayment += interestPayment;
+
+        // Проверка на -баланс
+        if (balance < 0) {
+            principalPayment += balance;
+            monthlyPayment = principalPayment + interestPayment;
+            balance = 0;
+        }
+
+        const row = document.createElement('tr');
+        row.innerHTML = '<td>' + i + '</td><td>' + initialBalance.toFixed(2) + '</td><td>' + principalPayment.toFixed(2) + '</td><td>' + interestPayment.toFixed(2) + '</td><td>' + monthlyPayment.toFixed(2) + '</td><td>' + balance.toFixed(2) + '</td>';
+        table.appendChild(row);
+    }
+
+    // Итого
+    const totalRow = document.createElement('tr');
+    totalRow.innerHTML = '<td>Итого</td><td></td><td>' + totalPrincipalPayment.toFixed(2) + '</td><td>' + totalInterestPayment.toFixed(2) + '</td><td>' + totalPayment.toFixed(2) + '</td><td></td>';
+    table.appendChild(totalRow);
+}
+
+/*     ʕ ᵔᴥᵔ ʔ - Этот мишка растроится если мой код скопируют     */
+
